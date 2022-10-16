567d53e7287a0c1952bf1d60edfd29db04b8bfe7
Henry Okonkwo <okonkwo.henry2012@gmail.com>
2022-10-16 12:51:36 +0200

    setup balmUI library add a spinner component from balmUI add custom styles for spinner component

--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
 import { createApp } from 'vue';
+import BalmUI from 'balm-ui';
+import BalmUIPlus from 'balm-ui/dist/balm-ui-plus';
+import 'balm-ui/dist/balm-ui.css';
 import App from './App.vue';
 import './registerServiceWorker';
 import router from './router';
@@ -8,5 +11,7 @@ const app = createApp(App);
 
 app.use(store);
 app.use(router);
+app.use(BalmUI); // Mandatory
+app.use(BalmUIPlus); // Optional
 
 app.mount('#app');
--- vue.config.js
+++ vue.config.js
@@ -15,6 +15,14 @@ module.exports = {
   },
   runtimeCompiler: true,
   // NOTE: set alias via `configureWebpack` or `chainWebpack`
+  configureWebpack: {
+    resolve: {
+      alias: {
+        'balm-ui-plus': 'balm-ui/dist/balm-ui-plus.js',
+        'balm-ui-css': 'balm-ui/dist/balm-ui.css',
+      },
+    },
+  },
   chainWebpack: (config) => {
     config.module
       .rule('supportChaining')
