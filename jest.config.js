42c6b3a260ccddf5c5505ed068d889c4267d4383
Henry Okonkwo <okonkwo.henry2012@gmail.com>
2022-10-17 14:55:45 +0200

    Add test for axiosConfig.spec Refactor jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,13 @@
 module.exports = {
-  preset: '@vue/cli-plugin-unit-jest',
+  moduleFileExtensions: ['js', 'jsx', 'json', 'vue'],
   transform: {
     '^.+\\.vue$': 'vue-jest',
+    '.+\\.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$':
+      'jest-transform-stub',
+    '^.+\\.(js|jsx)?$': 'babel-jest',
   },
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  transformIgnorePatterns: ['<rootDir>/node_modules/'],
 };
